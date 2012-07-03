78c60635557f785ce2f40630996c0d5095bc3e23
Robert Moore <me@robdmoore.id.au>
2012-07-03 22:52:49 +0800

    Added class to abstract away the RoleEnvironment checks to allow testing without having to run the emulator

---
+++ AzureWebFarm/Helpers/AzureRoleEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace AzureWebFarm.Helpers
+{
+    public static class AzureRoleEnvironment
+    {
+        public static Func<string> DeploymentId = () => RoleEnvironment.DeploymentId;
+        public static Func<string> CurrentRoleInstanceId = () => RoleEnvironment.CurrentRoleInstance.Id;
+        public static Func<string, string> GetConfigurationSettingValue = RoleEnvironment.GetConfigurationSettingValue;
+
+        public static bool IsComputeEmulatorEnvironment
+        {
+            get
+            {
+                return RoleEnvironment.IsAvailable && RoleEnvironment.DeploymentId.StartsWith("deployment", StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    }
+}
