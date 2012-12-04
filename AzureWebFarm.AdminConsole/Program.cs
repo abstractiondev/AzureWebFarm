c96a342b642294001fefe03ca0b5e2c1506acc17
Robert Moore <me@robdmoore.id.au>
2012-12-04 23:18:44 +0800

    Removing unused value

--- AzureWebFarm.AdminConsole/Program.cs
+++ AzureWebFarm.AdminConsole/Program.cs
@@ -11,7 +11,7 @@ namespace AzureWebFarm.AdminConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Write("Enter the storage account name: ");
             var accountName = Console.ReadLine();
