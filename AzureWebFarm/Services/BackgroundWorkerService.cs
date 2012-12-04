10cb15b0497c8877443e11efa8aaa57c1d78964d
Robert Moore <me@robdmoore.id.au>
2012-12-04 23:50:57 +0800

    Added skeleton for background worker service

---
+++ AzureWebFarm/Services/BackgroundWorkerService.cs
@@ -0,0 +1,18 @@
+﻿namespace AzureWebFarm.Services
+{
+    public class BackgroundWorkerService
+    {
+        public BackgroundWorkerService(string localPath, string executablePath)
+        {
+            // todo: scan for exes and build definition
+        }
+
+        public Update(string siteName)
+        {
+            // todo: is it a new site?
+            // todo: are there any console apps that aren't there any more?
+            // todo: are there any new console apps?
+            // todo: kill old one
+        }
+    }
+}
