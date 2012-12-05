3a1a1d808bf0518a15d83c56e3ebf18a765b6828
Robert Moore <me@robdmoore.id.au>
2012-12-06 00:06:27 +0800

    Added initial background worker service code and a test - badly needs refactoring, but at least the basic use case works

---
+++ AzureWebFarm.Tests/Services/BackgroundWorkerServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Reflection;
+using AzureWebFarm.Services;
+using NUnit.Framework;
+
+namespace AzureWebFarm.Tests.Services
+{
+    [TestFixture]
+    class BackgroundWorkerServiceShould
+    {
+        private static readonly string TestPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", ""));
+
+        private static readonly string SitesPath = Path.Combine(TestPath, "Sites");
+        private static readonly string ExePath = Path.Combine(TestPath, "Executables");
+
+        private static readonly string TestAppPath = "TestApp{0}";
+        private static readonly string TestAppExe = Path.Combine(TestAppPath, "TestApp{0}.exe");
+
+        private BackgroundWorkerService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            if (Directory.Exists(SitesPath))
+                Directory.Delete(SitesPath, true);
+
+            if (Directory.Exists(ExePath))
+                Directory.Delete(ExePath, true);
+
+            Directory.CreateDirectory(SitesPath);
+            Directory.CreateDirectory(ExePath);
+
+            _service = new BackgroundWorkerService(SitesPath, ExePath);
+        }
+
+        private void SetupTestApp(int app, string siteName)
+        {
+            Directory.CreateDirectory(Path.Combine(SitesPath, siteName, string.Format(TestAppPath, app)));
+            File.Copy(Path.Combine(TestPath, string.Format("TestApp{0}.exe", app)), Path.Combine(SitesPath, siteName, string.Format(TestAppExe, app)));
+        }
+
+        [Test]
+        public void Run_executable()
+        {
+            const string siteName = "test";
+            const int app = 1;
+            SetupTestApp(app, siteName);
+
+            _service.Update(siteName);
+
+            var outputFile = Path.Combine(ExePath, siteName, string.Format(TestAppPath, app), "file.txt");
+            Assert.That(File.Exists(outputFile));
+            Assert.That(File.ReadAllText(outputFile), Is.EqualTo(app.ToString()));
+        }
+    }
+}
--- AzureWebFarm/Services/BackgroundWorkerService.cs
+++ AzureWebFarm/Services/BackgroundWorkerService.cs
@@ -1,18 +1,55 @@
-﻿namespace AzureWebFarm.Services
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace AzureWebFarm.Services
 {
     public class BackgroundWorkerService
     {
+        private readonly string _localPath;
+        private readonly string _executablePath;
+
         public BackgroundWorkerService(string localPath, string executablePath)
         {
+            _localPath = localPath;
+            _executablePath = executablePath;
             // todo: scan for exes and build definition
         }
 
-        public Update(string siteName)
+        public void Update(string siteName)
         {
             // todo: is it a new site?
             // todo: are there any console apps that aren't there any more?
             // todo: are there any new console apps?
             // todo: kill old one
+
+            var subDirs = Directory.EnumerateDirectories(Path.Combine(_localPath, siteName));
+            foreach (var d in subDirs)
+            {
+                var subDir = d.Split(Path.DirectorySeparatorChar).Last();
+                var exe = Path.Combine(d, string.Format("{0}.exe", subDir));
+                if (!File.Exists(exe))
+                    continue;
+
+                var exeDir = Path.Combine(_executablePath, siteName, subDir);
+                var dest = Path.Combine(exeDir, string.Format("{0}.exe", subDir));
+                if (!Directory.Exists(exeDir))
+                    Directory.CreateDirectory(exeDir);
+                File.Copy(exe, dest, true);
+
+                var startInfo = new ProcessStartInfo(dest)
+                {
+                    WorkingDirectory = exeDir,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
+
+                using (var process = Process.Start(startInfo))
+                {
+                    process.WaitForExit();
+                }
+            }
         }
     }
 }
