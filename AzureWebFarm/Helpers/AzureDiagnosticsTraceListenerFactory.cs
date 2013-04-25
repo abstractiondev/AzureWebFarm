9ba85342aa8c61980fe63f192e31f80eb950d187
Robert Moore <me@robdmoore.id.au>
2013-04-25 20:29:04 +0800

    Updated the diagnostics trace listener factory to trace events properly rather than using Trace.Write

--- AzureWebFarm/Helpers/AzureDiagnosticsTraceListenerFactory.cs
+++ AzureWebFarm/Helpers/AzureDiagnosticsTraceListenerFactory.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using Castle.Core.Logging;
 
 namespace AzureWebFarm.Helpers
 {
+    /// <summary>
+    /// Castle.Core Logger Factory that provides a logger that uses Trace.Write in a way compatible with the Azure Diagnostics Listener.
+    /// </summary>
     public class AzureDiagnosticsTraceListenerFactory : AbstractLoggerFactory
     {
         public override ILogger Create(string name)
@@ -23,15 +23,67 @@ namespace AzureWebFarm.Helpers
     /// <summary>
     /// Redirects all Trace messages to the "Default" TraceSource so that the standard Azure Diagnostics Listener picks up the messages.
     /// </summary>
-    public class AzureDiagnosticsLogger : TraceLogger
+    public class AzureDiagnosticsLogger : LevelFilteredLogger
     {
-        public AzureDiagnosticsLogger(string name) : base(name) { }
+        private readonly TraceSource _traceSource;
+        public AzureDiagnosticsLogger(string name) : base(name)
+        {
+            _traceSource = new TraceSource("Default", MapSourceLevels(Level));
+        }
 
-        public AzureDiagnosticsLogger(string name, LoggerLevel level) : base(name, level) { }
+        public AzureDiagnosticsLogger(string name, LoggerLevel level) : base(name, level)
+        {
+            _traceSource = new TraceSource("Default", MapSourceLevels(Level));
+        }
+
+        public override ILogger CreateChildLogger(string loggerName)
+        {
+            return new AzureDiagnosticsLogger(loggerName, Level);
+        }
 
         protected override void Log(LoggerLevel loggerLevel, string loggerName, string message, Exception exception)
         {
-            Trace.Write(string.Format("{0}: {1}", loggerName, message), loggerLevel.ToString());
+            if (exception == null)
+                _traceSource.TraceEvent(MapTraceEventType(loggerLevel), 0, "[{0}] {1}", loggerName, message);
+            else
+                _traceSource.TraceData(MapTraceEventType(loggerLevel), 0, (object) string.Format("[{0}] {1}", loggerName, message), exception);
+        }
+
+        private static SourceLevels MapSourceLevels(LoggerLevel level)
+        {
+            switch (level)
+            {
+                case LoggerLevel.Debug:
+                    return SourceLevels.Verbose;
+                case LoggerLevel.Info:
+                    return SourceLevels.Information;
+                case LoggerLevel.Warn:
+                    return SourceLevels.Warning;
+                case LoggerLevel.Error:
+                    return SourceLevels.Error;
+                case LoggerLevel.Fatal:
+                    return SourceLevels.Critical;
+            }
+            return SourceLevels.Off;
+        }
+
+        private static TraceEventType MapTraceEventType(LoggerLevel level)
+        {
+            switch (level)
+            {
+                case LoggerLevel.Fatal:
+                    return TraceEventType.Critical;
+                case LoggerLevel.Error:
+                    return TraceEventType.Error;
+                case LoggerLevel.Warn:
+                    return TraceEventType.Warning;
+                case LoggerLevel.Info:
+                    return TraceEventType.Information;
+                case LoggerLevel.Debug:
+                    return TraceEventType.Verbose;
+                default:
+                    return TraceEventType.Verbose;
+            }
         }
     }
 }
