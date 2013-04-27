d7b2a32b99b61161497d6bcae2f047c1c4281095
Robert Moore <me@robdmoore.id.au>
2013-04-27 13:31:38 +0800

    Moved the azure role configuration code to a static class

---
+++ AzureWebFarm/Config/AzureConfig.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using AzureWebFarm.Helpers;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace AzureWebFarm.Config
+{
+    internal static class AzureConfig
+    {
+        private const string TempLocalResource = "TempSites";
+        private const string SitesLocalResource = "Sites";
+        private const string ExecutionLocalResource = "Execution";
+
+        public static void ConfigureRole()
+        {
+            // Allow multiple simultaneous HTTP request threads
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // Allow Azure Storage to always use the latest version of a config setting
+            CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) =>
+            {
+                if (!AzureRoleEnvironment.IsAvailable())
+                {
+                    configSetter(ConfigurationManager.AppSettings[configName]);
+                    return;
+                }
+
+                configSetter(AzureRoleEnvironment.GetConfigurationSettingValue(configName));
+                // Apply any changes to config when the config is edited http://msdn.microsoft.com/en-us/library/windowsazure/gg494982.aspx
+                AzureRoleEnvironment.Changed += (sender, arg) =>
+                {
+                    if (!arg.Changes.OfType<RoleEnvironmentConfigurationSettingChange>().Any(change => (change.ConfigurationSettingName == configName)))
+                        return;
+
+                    if (!configSetter(AzureRoleEnvironment.GetConfigurationSettingValue(configName)))
+                        AzureRoleEnvironment.RequestRecycle();
+                };
+            });
+
+            // Configure local resources
+            var localTempPath = GetLocalResourcePathAndSetAccess(TempLocalResource);
+            GetLocalResourcePathAndSetAccess(SitesLocalResource);
+            GetLocalResourcePathAndSetAccess(ExecutionLocalResource);
+            
+            // WebDeploy creates temporary files during package creation. The default TEMP location allows for a 100MB
+            // quota (see http://msdn.microsoft.com/en-us/library/gg465400.aspx#Y976). 
+            // For large web deploy packages, the synchronization process will raise an IO exception because the "disk is full" 
+            // unless you ensure that the TEMP/TMP target directory has sufficient space
+            Environment.SetEnvironmentVariable("TMP", localTempPath);
+            Environment.SetEnvironmentVariable("TEMP", localTempPath);
+        }
+
+        public static string GetTempLocalResourcePath()
+        {
+            return GetLocalResourcePath(TempLocalResource);
+        }
+
+        public static string GetSitesLocalResourcePath()
+        {
+            return GetLocalResourcePath(SitesLocalResource);
+        }
+
+        public static string GetExecutionLocalResourcePath()
+        {
+            return GetLocalResourcePath(ExecutionLocalResource);
+        }
+
+        private static string GetLocalResourcePath(string localResourceName)
+        {
+            return AzureRoleEnvironment.GetLocalResource(localResourceName).RootPath.TrimEnd('\\');
+        }
+
+        private static string GetLocalResourcePathAndSetAccess(string localResourceName)
+        {
+            var resourcePath = GetLocalResourcePath(localResourceName);
+
+            var localDataSec = Directory.GetAccessControl(resourcePath);
+            localDataSec.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+            Directory.SetAccessControl(resourcePath, localDataSec);
+
+            return resourcePath;
+        }
+    }
+}
--- AzureWebFarm/WebFarmRole.cs
+++ AzureWebFarm/WebFarmRole.cs
@@ -1,20 +1,14 @@
 ﻿using System;
-using System.Configuration;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Security.AccessControl;
-using System.Security.Principal;
 using System.Threading;
 using AzureToolkit;
+using AzureWebFarm.Config;
 using AzureWebFarm.Helpers;
 using AzureWebFarm.Services;
 using AzureWebFarm.Storage;
 using Castle.Core.Logging;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
-using Microsoft.WindowsAzure.ServiceRuntime;
 
 namespace AzureWebFarm
 {
@@ -49,47 +43,14 @@ namespace AzureWebFarm
         {
             try
             {
-                // Set-up diagnostics
                 DiagnosticsHelper.ConfigureDiagnosticMonitor(_diagnosticsLogLevel);
                 _logger.Info("WebRole.OnStart called");
-
-                ServicePointManager.DefaultConnectionLimit = 12;
-
-                // Allow Azure Storage to always use the latest version of a config setting
-                CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) =>
-                    {
-                        if (!AzureRoleEnvironment.IsAvailable())
-                        {
-                            configSetter(ConfigurationManager.AppSettings[configName]);
-                            return;
-                        }
-
-                        configSetter(AzureRoleEnvironment.GetConfigurationSettingValue(configName));
-                        // Apply any changes to config when the config is edited http://msdn.microsoft.com/en-us/library/windowsazure/gg494982.aspx
-                        AzureRoleEnvironment.Changed += (sender, arg) =>
-                        {
-                            if (!arg.Changes.OfType<RoleEnvironmentConfigurationSettingChange>().Any(change => (change.ConfigurationSettingName == configName)))
-                                return;
-
-                            if (!configSetter(AzureRoleEnvironment.GetConfigurationSettingValue(configName)))
-                                AzureRoleEnvironment.RequestRecycle();
-                        };
-                    }
-                );
-
-                // Initialize local resources
-                var localSitesPath = GetLocalResourcePathAndSetAccess("Sites");
-                var localTempPath = GetLocalResourcePathAndSetAccess("TempSites");
-                var localExecutionPath = GetLocalResourcePathAndSetAccess("Execution");
-
-                // WebDeploy creates temporary files during package creation. The default TEMP location allows for a 100MB
-                // quota (see http://msdn.microsoft.com/en-us/library/gg465400.aspx#Y976). 
-                // For large web deploy packages, the synchronization process will raise an IO exception because the "disk is full" 
-                // unless you ensure that the TEMP/TMP target directory has sufficient space
-                Environment.SetEnvironmentVariable("TMP", localTempPath);
-                Environment.SetEnvironmentVariable("TEMP", localTempPath);
+                AzureConfig.ConfigureRole();
 
                 // Create the sync service and background worker
+                var localTempPath = AzureConfig.GetTempLocalResourcePath();
+                var localSitesPath = AzureConfig.GetSitesLocalResourcePath();
+                var localExecutionPath = AzureConfig.GetExecutionLocalResourcePath();
                 var storageAccount = CloudStorageAccount.FromConfigurationSetting(Constants.StorageConnectionStringKey);
                 var storageFactory = new AzureStorageFactory(storageAccount);
                 var websiteRepository = new WebSiteRepository(storageFactory);
@@ -152,17 +113,6 @@ namespace AzureWebFarm
                     break;
                 Thread.Sleep(TimeSpan.FromSeconds(1));
             }
-        }
-        
-        private static string GetLocalResourcePathAndSetAccess(string localResourceName)
-        {
-            var resourcePath = AzureRoleEnvironment.GetLocalResource(localResourceName).RootPath.TrimEnd('\\');
-
-            var localDataSec = Directory.GetAccessControl(resourcePath);
-            localDataSec.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
-            Directory.SetAccessControl(resourcePath, localDataSec);
-
-            return resourcePath;
         }
     }
 }
